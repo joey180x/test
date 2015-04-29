259cdec60dc8273d611645131a1ba99a9d2121a2
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-29 11:51:50 -0700

    Finished Quiz 100%

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$( ".Dan" ).click(function() {
+	//when the div Dan geets clicked
+    alert("Hey you clicked on me.");
+    //calling placeholder text line 1
+});
+
+
+$( ".Iwao" ).click(function() {
+	$( ".Iwao" ).hide();
+ 
+});
